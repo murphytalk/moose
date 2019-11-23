a83fbb423ddbbbefa777889b892b326ec0b584f6
Mu Lu <murphytalk@gmail.com>
2019-11-23 22:39:02 +0900

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# What is this
+
+This is a standalone app to play with my PoC ideas.
+
+It is going to be the workhorse of my many ideas, so I intend to name it after a big animal that is not as boring as a horse. Since I choose to use [kotlin](https://kotlinlang.org/) to code, I don't know if [Kotlin Island](https://en.wikipedia.org/wiki/Kotlin_Island) actually has reindeer, but hey it is a big animal that is north enough to me ;)
+
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,11 @@
  */
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_12
+    targetCompatibility = JavaVersion.VERSION_12
+}
+
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.41"
@@ -22,12 +27,9 @@ repositories {
 }
 
 dependencies {
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
@@ -43,7 +45,7 @@ val shadowJar: ShadowJar by tasks
 
 // also see: https://github.com/csolem/gradle-shadow-jar-with-kotlin-dsl/blob/master/build.gradle.kts
 shadowJar.apply {
-    archiveBaseName.set("reindeer-all")
+    archiveBaseName.set("reindeer")
     archiveVersion.set(rel)
 }
 
